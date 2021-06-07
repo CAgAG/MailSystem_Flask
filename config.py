0dae092633944cb75742db2c361ddfd919249a4f
CAgAG <34125090+CAgAG@users.noreply.github.com>
2021-06-07 21:16:18 +0800

    Update config.py

--- config.py
+++ config.py
@@ -2,9 +2,9 @@
 # MAIL_SERVER = "smtp.163.com"
 # MAIL_PORT = "587"
 # MAIL_USE_TLS = True
-# MAIL_USERNAME = "15685134992@163.com"
-# MAIL_PASSWORD = "HSJKBVWZUUKPOOOK" #生成的授权码
-# MAIL_DEFAULT_SENDER = "15685134992@163.com"
+# MAIL_USERNAME = "xx@163.com"
+# MAIL_PASSWORD = "xx" #生成的授权码
+# MAIL_DEFAULT_SENDER = "xx@163.com"
 
 # MAIL_SERVER	localhost	电子邮件服务器的主机名或IP地址
 # MAIL_PORT	587	电子邮件服务器的端口
